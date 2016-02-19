ea51bf6eb81924bf0175a8d8b4f3c1272a965044
WuZhen <wuzhen@jidemail.com>
2016-02-19 16:13:07 +0800

    add Android.mk to build for android
    NO_REF_TASK
    tested: local run
    
    Change-Id: Iecc448858a5adbd6aa48326c6ba4d94c6ed79e5c

---
+++ Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	s2tc_libtxc_dxtn.cpp \
+	s2tc_algorithm.cpp \
+
+LOCAL_C_INCLUDES := \
+	external/mesa/include \
+
+LOCAL_MODULE := libtxc_dxtn
+
+include $(BUILD_SHARED_LIBRARY)
